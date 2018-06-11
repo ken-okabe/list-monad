33ecb6488d357dfab917957e67d2b77b9e98fa68
kenokabe <kenokabe@gmail.com>
2018-06-11 09:57:34 +0900

    commit

--- index.js
+++ index.js
@@ -34,72 +34,33 @@
     })();
     return M;
   })();
+  //========================================================
   const _listMonad = () => freeMonoid(operator);
   const operator = list => {
-
-
     const M = list.M;
     const toList = arr => arr.reduce((a, b) => (a)(b), (M));
-
     list.fold = (op) => { //===========================
       //  list1 = fold(list)(op);----------------------
-      /*      console.log("NOW FOLDING-------------------");
-            console.log(list);
-            console.log("-----------------NOW FOLDING--");
-      */
       const fold = list => op => [M, ...list.units]
         .reduce((a, b) => {
-          /*      console.log("FOLDING=========================================");
-                console.log(a);
-                console.log(b);
-                console.log("a.Val=================");
-                console.log(a.val);
-*/
           const aVal = (a.val.length === 1)
-            ? a.val[0]
-            : a.val;
+            ? a.val[0] : a.val;
           const a1Val = b.val.map(bVal => op(aVal, bVal))[0];
-
-          /*
-                    console.log("aVal1=================");
-                    console.log(a1Val);
-          */
           //identity * monoid should be monoid
           const err = (!!a.identity) && (!a1Val.M);
-
-          /*        console.log("error=================");
-
-                  console.log(err);
-*/
-          const a1Val2 = err
-            ? b
-            : a1Val;
-
+          const a1Val2 = err ? b : a1Val;
           const a1 = (M)(a1Val2);
-          /*        console.log("a1=================");
-                  console.log(a1);
-
-                  console.log((M)(a1Val));
-*/
           return a1;
-
-        });
-
-      return fold(list)((M)(op).val[0]); //wrap and val for op
-    //--------------------------------
+        }); //wrap and val for op
+      return fold(list)((M)(op).val[0]);
     }; //===============================================
     const mapOp = f => (a, b) => (M)(a)(f(b));
     list.fmap = (f) => {
       const f1 = (M)(f).val[0]; //list and val
       return list.fold(mapOp(f1));
     };
-
-    //  const compose = (f, g) => (x => g(f(x)));
-
-  //  const f = (M)(ff).fold(compose).val[0];
   };
   const listMonad = _listMonad();
-
   //------------------
   const exporting = (typeof module === "object"
   && typeof module.exports === "object")
