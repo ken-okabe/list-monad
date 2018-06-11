4a45e1fb639b35b866befbc9870675e6dcdc733e
kenokabe <kenokabe@gmail.com>
2018-06-11 09:53:37 +0900

    commit

--- index.js
+++ index.js
@@ -1,86 +1,98 @@
 (() => {
   "use strict";
   const freeMonoid = (operator) => (() => {
+    Array.prototype.flatten = function() {
+      return Array.prototype.concat.apply([], this);
+    };
     const M = (() => { //(M)(a)(b)
-      const m = (a) => (!!a && (!!a.M || a.identity)) //left id M
-        ? (a)
-        : (() => {
-          const ma = b => (b.identity) //right id M
-            ? (ma)
-            : !b.M
-              ? (ma)(M(b))
-              : (() => {
-                const mab = M();
-                const [m, ...bUnits] = b.units;
-                mab.units = ma.units.concat(bUnits);
-                mab.val = mab.units.map(unit => unit.val[0]);
-                return mab; // (m)(a)(b)
-              })();
-          ma.val = [a];
-          ma.M = m;
-          ma.units = m.units.concat(ma);
-          operator(ma);
-          return ma;
-        })();
+      const toList = arr => arr.reduce((a, b) => (a)(b), (M));
+      const m = (a) => (Array.isArray(a))
+        ? toList(a.flatten())
+        : (!!a && (!!a.M || a.identity)) //left id M
+          ? (a)
+          : (() => {
+            const ma = b => (b.identity) //right id M
+              ? (ma)
+              : !b.M
+                ? (ma)(M(b))
+                : (() => {
+                  const mab = M();
+                  mab.units = ma.units.concat(b.units);
+                  mab.val = mab.units.map(unit => unit.val[0]);
+                  return mab; // (m)(a)(b)
+                })();
+            ma.val = [a];
+            ma.M = m;
+            ma.units = [ma];
+            operator(ma);
+            return ma;
+          })();
       m.identity = true;
-      m.val = ["__IDENTITY__"];
+      m.val = [m]; //["__IDENTITY__"];
       m.units = [m];
       return m;
     })();
     return M;
   })();
-
   const _listMonad = () => freeMonoid(operator);
   const operator = list => {
+
+
     const M = list.M;
     const toList = arr => arr.reduce((a, b) => (a)(b), (M));
 
     list.fold = (op) => { //===========================
       //  list1 = fold(list)(op);----------------------
-      const fold = list => op => (list.units.length === 2)
-        ? list
-        : (() => {
-          console.log("list=================");
+      /*      console.log("NOW FOLDING-------------------");
+            console.log(list);
+            console.log("-----------------NOW FOLDING--");
+      */
+      const fold = list => op => [M, ...list.units]
+        .reduce((a, b) => {
+          /*      console.log("FOLDING=========================================");
+                console.log(a);
+                console.log(b);
+                console.log("a.Val=================");
+                console.log(a.val);
+*/
+          const aVal = (a.val.length === 1)
+            ? a.val[0]
+            : a.val;
+          const a1Val = b.val.map(bVal => op(aVal, bVal))[0];
 
-          console.log(list);
-          const [a, b, ...rest] = list.units;
+          /*
+                    console.log("aVal1=================");
+                    console.log(a1Val);
+          */
+          //identity * monoid should be monoid
+          const err = (!!a.identity) && (!a1Val.M);
 
-          console.log("a  b  ===============");
-          console.log(a.val[0]);
-          console.log(b.val[0]);
+          /*        console.log("error=================");
 
-          const a1 = (a.identity)
-            ? a
-            : a.val[0];
+                  console.log(err);
+*/
+          const a1Val2 = err
+            ? b
+            : a1Val;
 
-          const b1 = b.val[0];
+          const a1 = (M)(a1Val2);
+          /*        console.log("a1=================");
+                  console.log(a1);
 
-          const new0 = op(a1, b1);
-          console.log("new0=================");
+                  console.log((M)(a1Val));
+*/
+          return a1;
 
-          console.log(new0);
+        });
 
-          const list1 = ((a.identity) && (!new0.M))
-            ? (() => {
-              const [c, ...rest1] = rest;
-              const c1 = c.val[0];
-              const new1 = op(b1, c1);
-              return toList([M(new1), ...rest1])
-            })() //(M)  identiyString123
-            : toList([M(new0), ...rest]);
-
-          console.log(list1);
-
-          //    return fold(list1)(op); //need TailCallOptimization
-          return fold(M(1)(2));
-        })();
-        //----------------------------------
-
-      return fold(list)(op);
+      return fold(list)((M)(op).val[0]); //wrap and val for op
     //--------------------------------
     }; //===============================================
-    const mapOp = f => ((a, b) => (M)(a)(f(b)));
-    list.fmap = (f) => list.fold(mapOp(f));
+    const mapOp = f => (a, b) => (M)(a)(f(b));
+    list.fmap = (f) => {
+      const f1 = (M)(f).val[0]; //list and val
+      return list.fold(mapOp(f1));
+    };
 
     //  const compose = (f, g) => (x => g(f(x)));
 
--- test.js
+++ test.js
@@ -3,7 +3,6 @@
 
   const M = require("./index");
 
-
   const mlog = m => o => {
     console.log(m + "\n" + o.val);
     return o;
@@ -19,113 +18,104 @@
     xyz
   );
 
-  /*
-    mlog("--fold----")(
-      (M)(1)(2)(9)
-        .fold((a, b) => a + b)
-    );
-  */
+  mlog("--fold----")(
+    (M)(1)(2)(9)
+      .fold((a, b) => a + b)
+  );
+
   console.log("+++++++++++++++++++");
+
+  mlog("fold to map")(
+    (M)(10)(20)(30)(40)
+      .fold((a, b) => (M)(a)(b * 2))
+  );
+
+
 
   mlog("--fold- MM---")(
     (M)(10)(20)(30)(40)
-      .fold((a, b) => (a)(b))
+      .fold((a, b) => (M)(a)(b)(a)(b))
   );
 
-  /*
-    mlog("--fold- MM---")(
-  (M)(10)(20)(30)(40)
-    .fold((a, b) => (a)(b)(a)(b))
-    );
-
-    mlog("--fold- MM---")(
-  (M)(1)(1)
-    .fold((a, b) => (a)(a + b))
-    );
 
 
-    mlog("--fold MM2----")(
-  (M)(10)(20)(30)(40)
-    .fold((a, b) => (a )(b * 2))
-    );
-    /*
-    const add1 = (a) => (a + 1);
+  const add1 = (a) => (a + 1);
 
-    mlog("---fmap---")(
-      (M)(9).fmap(add1)
-    );
+  mlog("---fmap---")(
+    (M)(9).fmap(add1)
+  );
 
-    mlog("------")(
-      (M)(999)(9).fmap(add1)
-    );
-    mlog("--fmap----")(
-      (M)(9)(8)
-        .fmap((a) => (a))
-    );
-    const add5 = (a => a + 5);
-    mlog("--fmap5----")(
-      (M)(100)(101)
-        .fmap(add5)
-    );
-    const add10 = a => a + 5;
-    mlog("--fmap10----")(
-      (M)(100)(101)
-        .fmap(add5)
-        .fmap(add5)
-    );
-    mlog("------")(
-      (M)(add1)(add1).fmap(f => f(3)) //
-    );
-    mlog("------")(
-      (M)(add1).fmap(f => f(3)) //4
-    );
+  mlog("------")(
+    (M)(999)(9).fmap(add1)
+  );
+  mlog("--fmap----")(
+    (M)(9)(8)
+      .fmap((a) => (a))
+  );
+  const add5 = (a => a + 5);
+  mlog("--fmap5----")(
+    (M)(100)(101)
+      .fmap(add5)
+  );
+  const add10 = a => a + 5;
+  mlog("--fmap10----")(
+    (M)(100)(101)
+      .fmap(add5)
+      .fmap(add5)
+  );
+  mlog("------")(
+    (M)(add1)(add1).fmap(f => f(3)) //
+  );
+  mlog("------")(
+    (M)(add1).fmap(f => f(3)) //4
+  );
 
 
 
-    mlog("------")(
-      (M)(9).fmap(x => x)
-    );
+  mlog("------")(
+    (M)(9).fmap(x => x)
+  );
 
-    console.log("------");
+  console.log("------");
 
 
-    const double = (a) => (M)(a)(a);
+  const double = (a) => (M)(a)(a);
 
-    mlog("xyz--fmap------")(
-      xyz
-        .fmap(double)
-        .fmap(double)
-        .fmap(add1)
-    );
+  mlog("xyz--fmap------")(
+    xyz
+      .fmap(double)
+      .fmap(double)
+      .fmap(add1)
+  );
 
-    const compose = (f, g) => (x => g(f(x)));
-    const add20 = x => x + 20;
-    //console.log(m);
-    mlog("--fold--compose 0--")(
-      (M)(100).fmap(add20)
-    );
+  const compose = (f, g) => (x => g(f(x)));
+  const add20 = x => x + 20;
+  //console.log(m);
+  mlog("--fold--compose 0--")(
+    (M)(100).fmap(add20)
+  );
 
-    mlog("--fold--compose--")(
-      (M)(100).fmap(
-        M(add20)(add20)(add20)
-          .fold(compose).val[0])
-    );
+  mlog("--fold--compose--")(
+    (M)(100)(200).fmap(
+      M(add20)(add20)(add20).fold(compose)
+    )
+  );
 
-    /*
-      const plus = (x) => (y => x + y);
-      const plus1 = (M)(1)
-        .fmap(plus);
 
-      mlog("--123 p1 p1----")(
-        (M)(1)(2)(3)
-          .fmap(plus1)
-      );
-      mlog("--fold----")(
-        (M)(1)(2)(3)
-          .fmap((plus1)(plus1))
-          .fold((a, b) => (a + b))
-      );
-    */
+  const plus = (x) => (y => x + y);
+  const plus1 = (M)(1)
+    .fmap(plus);
+
+  mlog("--123 p1 p1----")(
+    (M)(1)(2)(3)
+      .fmap(plus1)
+  );
+  mlog("--fold----")(
+    (M)(1)(2)(3)
+      .fmap(plus1)
+      .fold((a, b) => (a + b))
+  );
+
   /*
     console.log(
       [1].reduce((a, b) => (a * a + a * b + b))
